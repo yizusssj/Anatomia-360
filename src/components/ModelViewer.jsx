939054be5380706edab9ab5b31800c1461de8498
Yisus <yisus@gmail.com>
2026-04-25 19:33:32 -0700

    update

--- src/components/ModelViewer.jsx
+++ src/components/ModelViewer.jsx
@@ -4,20 +4,31 @@ import { OrbitControls } from "@react-three/drei";
 function TestCube() {
   return (
     <mesh rotation={[0.4, 0.6, 0]}>
-      <boxGeometry args={[1.5, 1.5, 1.5]} />
-      <meshStandardMaterial color="#9ecbff" />
+      <boxGeometry args={[1.55, 1.55, 1.55]} />
+      <meshStandardMaterial color="#b9d4f0" metalness={0.12} roughness={0.45} />
     </mesh>
   );
 }
 
 export default function ModelViewer() {
   return (
-    <div className="w-full h-full rounded-2xl overflow-hidden">
+    <div className="w-full h-full">
       <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
-        <ambientLight intensity={1} />
-        <directionalLight position={[3, 3, 3]} intensity={1.5} />
+        <color attach="background" args={["#0d131d"]} />
+        <ambientLight intensity={1.15} />
+        <directionalLight position={[3, 3, 3]} intensity={1.8} />
+        <directionalLight position={[-2, 2, 4]} intensity={0.9} />
+        <pointLight position={[0, -2, 3]} intensity={0.6} />
+
         <TestCube />
-        <OrbitControls enablePan enableZoom enableRotate />
+
+        <OrbitControls
+          enablePan
+          enableZoom
+          enableRotate
+          minDistance={2.5}
+          maxDistance={8}
+        />
       </Canvas>
     </div>
   );
